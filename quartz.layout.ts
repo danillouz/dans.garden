3aa672a8d10fcccae7c5cffa04d4dab8f130ddfb
Daniël Illouz <danillouz@users.noreply.github.com>
2024-07-29 23:05:23 +0200

    Update layouts for content and folder/tags pages
    - Make explorer index links clickable
    - Fix layout inconsistencies to prevent the layout from "jumping"
    - Use custom breadcrumbs spacer symbol

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,7 +14,10 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({
+      spacerSymbol: "/",
+      hideOnRoot: false,
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -24,7 +27,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        folderDefaultState: "open",
+      }),
+    ),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
@@ -35,13 +43,25 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs({
+      spacerSymbol: "/",
+      hideOnRoot: false,
+    }),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        folderDefaultState: "open",
+      }),
+    ),
   ],
-  right: [],
+  right: [Component.Backlinks(), Component.Graph()],
 }
