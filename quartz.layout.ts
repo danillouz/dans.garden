5fcb43a343facf594665d52d3355e49b165883de
Daniël Illouz <danillouz@users.noreply.github.com>
2024-08-06 21:40:00 +0200

    Remove `PageTitle`
    Just so I have a clean diff, in case I change my mind.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
@@ -64,7 +63,6 @@ export const defaultListPageLayout: PageLayout = {
     Component.ContentMeta(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
